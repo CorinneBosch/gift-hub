7d30bada0fed6d85d8851e3c939e006d5e6a9b70
Farya <farya.hsn@gmail.com>
2021-10-07 14:42:03 +0100

    added connection to mongoose db

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 
 require("dotenv").config();
 
@@ -8,6 +9,16 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+const uri =
+  "mongodb+srv://team-work-social:1234@cluster0.cfu7c.mongodb.net/work-social?retryWrites=true&w=majority";
+
+mongoose.connect(uri);
+const connection = mongoose.connection;
+connection.on("error", console.error.bind(console, "connection error: "));
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully");
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);

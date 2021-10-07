9abb74f47662578603c9b4daffabd9301ab56e7f
Corinne <corinne.boesch@gmail.com>
2021-10-07 16:14:37 +0100

    extracted uri route to .env file and requiring constant / added user routes

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
-const express = require("express");
-const cors = require("cors");
-const mongoose = require("mongoose");
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
 
-require("dotenv").config();
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,15 +10,16 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const dbURI =
-  "mongodb+srv://team-work-social:1234@cluster0.cfu7c.mongodb.net/work-social?retryWrites=true&w=majority";
-
-mongoose.connect(dbURI);
+mongoose.connect(proccess.env.DB_URI);
 const connection = mongoose.connection;
-connection.on("error", console.error.bind(console, "connection error: "));
-connection.once("open", () => {
-  console.log("MongoDB database connection established successfully");
+connection.on('error', console.error.bind(console, 'connection error: '));
+connection.once('open', () => {
+  console.log('MongoDB database connection established successfully');
 });
+
+const usersRouter = require('./routes/users');
+
+app.use('/users', usersRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);

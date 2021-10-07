c0014d8c537082053e7ef746ae87410390ba7b76
Farya <farya.hsn@gmail.com>
2021-10-07 14:47:44 +0100

    add .env file

--- server/server.js
+++ server/server.js
@@ -10,10 +10,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri =
-  "mongodb+srv://team-work-social:1234@cluster0.cfu7c.mongodb.net/work-social?retryWrites=true&w=majority";
-
-mongoose.connect(uri);
+mongoose.connect(process.env.dbURI);
 const connection = mongoose.connection;
 connection.on("error", console.error.bind(console, "connection error: "));
 connection.once("open", () => {

8eebca1519c401eb66acd70f53471e1d2925de9a
Suhani Zaman <suhanizaman15@gmail.com>
2021-10-08 19:39:39 +0400

    user database test[endpoint test]

--- server/__tests__/createUser.spec.js
+++ server/__tests__/createUser.spec.js
@@ -1,7 +1,17 @@
 const router = require('../routes/Users.js');
+const app = require('../server.js');// Link to your server file
+const supertest = require("supertest");
+const request = supertest(app);
+
+
+// app.get("/", async (req, res) => {
+//   res.json([]);
+// });
 
 describe('User', () => {
-  it('whatever', async (done) => {
+  it("get the user test endpoints", async done => {
+    const res = await request.get("/users");
+    expect(res.status).toBe(200)
     done();
   });
 });

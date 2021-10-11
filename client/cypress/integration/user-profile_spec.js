c3856c1a24851f4a5e4fb74ec0fa93b3d9ffdba0
Hotu Nga Morehu Elkington <hotunz@gmail.com>
2021-10-11 13:20:09 +0100

    HE - Adding gift form test

--- client/cypress/integration/user-profile_spec.js
+++ client/cypress/integration/user-profile_spec.js
@@ -9,5 +9,14 @@ describe('UserProfile', () => {
       cy.get('.profile_pic_section').should('exist')
       cy.get('.payment_section').should('exist')
     })
+
+    it('has a pop up form that allows you to send a message allong with a donation', () => {
+      cy.contains('Buy me beer').click()
+      cy.get('.gift_form_name').type('Mighty Mouse');
+      cy.get('.gift_form_email').type('m1ghty_mouse@example.com');
+      cy.get('.gift_form_message').type("Here's a truce gift, Oil Can Harry");
+      cy.contains('Send').click();
+      cy.get('._modal-close').click();
+    })
   })
 })

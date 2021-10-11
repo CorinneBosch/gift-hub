4747c0f823399b55b9066fe2ce47c899dfc952d8
Suhani Zaman <suhanizaman15@gmail.com>
2021-10-11 19:01:37 +0400

    user test in progress

--- server/routes/users.js
+++ server/routes/users.js
@@ -7,7 +7,7 @@ router.route('/').get((req, res) => {
     .catch((err) => res.status(400).json('Error: ' + err));
 });
 
-router.route('/add').post((req, res) => {
+router.route('/register').post((req, res) => {
   const username = req.body.username;
   const email = req.body.email;
   const password = req.body.password;

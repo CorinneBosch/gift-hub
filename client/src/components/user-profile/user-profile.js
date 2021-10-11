75b2746434cb26e850b72480193cd7b0ced77214
Hotu Nga Morehu Elkington <hotunz@gmail.com>
2021-10-11 12:48:42 +0100

    HE - Adding profile layout

--- client/src/components/user-profile/user-profile.js
+++ client/src/components/user-profile/user-profile.js
@@ -12,10 +12,23 @@ const UserProfile = () => {
     <div className='title_section user_profile_title'>
       <div className="userProfile">
         <div className='profile_pic_section'>
+
+          {/* Profile pic placeholder begin */}
+
           <h1>This is the Profile picture section</h1>
+          <img src="https://image.shutterstock.com/image-vector/default-profile-picture-avatar-photo-260nw-1681253560.jpg" width='262px' alt='this is a placeholder' height='262px' border-radius='50%'/>
+          
+          {/* Profile pic placeholder end */}
+
         </div>
         <div className='bio_section'>
+
+        {/* Bio placeholder begin */}
+
           <h1>This is the Bio section</h1>
+
+        {/* Bio pic placeholder begin */}
+
         </div>
         <div className='payment_section'>
           <Container PaymentButtonText={PaymentButtonText} onSubmit={onSubmit} />

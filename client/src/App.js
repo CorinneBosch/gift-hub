4a8b66c410f38deb6400cfa0aab5cea000b7a313
Suhani Zaman <suhanizaman15@gmail.com>
2021-10-12 23:02:51 +0400

    navbar complied inside component dir

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,20 @@
 import "./App.css";
-import {Link} from 'react-router-dom';
+
 import UserProfile from "./components/user-profile/user-profile.js";
 
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import EditUser from "./components/edit-user.component";
 import CreateUser from "./components/create-user.components.js";
-
+import Navbar from "./components/navbar.js";
 
 
 function App() {
   return (
     <Router>
-      <div className="container">
-          <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            {/* <a class="navbar-brand" href="https://codingthesmartway.com" target="_blank">
-              <img src={logo} width="30" height="30" alt="CodingTheSmartWay.com" />
-            </a> */}
-            <Link to="/" className="navbar-brand">ONLYGIFT</Link>
-            <div className="collpase navbar-collapse">
-              <ul className="navbar-nav mr-auto">
-                <li className="navbar-item">
-                  <Link to="/signup" className="nav-link">Register</Link>
-                </li>
-                <li className="navbar-item">
-                  <Link to="/login" className="nav-link">Login</Link>
-                </li>
-              </ul>
-            </div>
-          </nav>
-          <br/>
+      < Navbar />
          <Route path="/signup" component={CreateUser} />
          <Route path="/edit/:id" component={EditUser} />
          <Route path="/profile/:userId" component={UserProfile} />
-      </div>
      </Router>
   );
 }
--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
-
+import {Link} from 'react-router-dom';
 export default class Navbar extends Component {
   render() {
     return (
-      <div>
-        <p>You are on the Edit User component!</p>
+      <div className="container">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+          {/* <a class="navbar-brand" href="https://codingthesmartway.com" target="_blank">
+            <img src={logo} width="30" height="30" alt="CodingTheSmartWay.com" />
+          </a> */}
+          <Link to="/" className="navbar-brand">ONLYGIFT</Link>
+          <div className="collpase navbar-collapse">
+            <ul className="navbar-nav mr-auto">
+              <li className="navbar-item">
+                <Link to="/signup" className="nav-link">Register</Link>
+              </li>
+              <li className="navbar-item">
+                <Link to="/login" className="nav-link">Login</Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <br/>
       </div>
     );
   }

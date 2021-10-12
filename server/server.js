1fcabdb7f5a5becf38b036797eeb891e0bf153de
Hotu Nga Morehu Elkington <hotunz@gmail.com>
2021-10-12 12:46:39 +0100

    HE - Adding base setup for deployment

--- server/server.js
+++ server/server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const path = require('path');
 
 require('dotenv').config();
 
+const usersRouter = require('./routes/users');
+
+
 const app = express();
-const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -17,12 +20,11 @@ connection.once('open', () => {
   console.log('MongoDB database connection established successfully');
 });
 
-const usersRouter = require('./routes/users');
-
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+// Step 2:
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
 app.use('/users', usersRouter);
-
-// app.listen(port, () => {
-//   console.log(`Server is running on port: ${port}`);
-// });
 
 module.exports = app;

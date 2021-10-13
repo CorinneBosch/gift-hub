73a3828e5317e1addd2eb5c959983702e066c383
Farya <farya.hsn@gmail.com>
2021-10-13 20:21:19 +0100

    added dropdown

--- client/src/components/user-profile/user-profile.js
+++ client/src/components/user-profile/user-profile.js
@@ -1,52 +1,103 @@
 import React from "react";
 import Container from "./form/container.js";
-import axios from 'axios';
-import Cookies from 'js-cookie';
+import axios from "axios";
+import Cookies from "js-cookie";
 
-const UserProfile = () => {
-  const PaymentButtonText = "Buy me beer";
-  const EditButtonText = 'Edit profile';
+class UserProfile extends React.Component {
+  constructor(props) {
+    // const [PaymentButtonText, buttonRef, showModal] = React.useState();
+    super(props);
+    this.state = { value: "coconut" };
 
-  const Username = Cookies.get('username')
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+    alert("Your gift is " + this.state.item + " :)");
+    event.preventDefault();
+  }
+
   // const UserId = Cookies.get('id')
-  
-  const onSubmit = (event) => {
+
+  onSubmit = (event) => {
     event.preventDefault(event);
     // console.log(event.target.email.value);
     // console.log(event.target.message.value);
   };
-  return (
-    <div className='title_section user_profile_title'>
-      <div className="userProfile">
-        <div id='edit_profile' className='edit_profile_section'>
-        <Container buttonText={EditButtonText} onSubmit={onSubmit}/>
-        </div>
-        <div className='profile_pic_section'>
-          <h1>Hello, {Username}</h1>
-          {console.log(document.cookie)}
-          {/* Profile pic placeholder begin */}
 
-          <h1>This is the Profile picture section</h1>
-          <img src="https://image.shutterstock.com/image-vector/default-profile-picture-avatar-photo-260nw-1681253560.jpg" width='262px' alt='this is a placeholder' height='262px' border-radius='50%'/>
-          
-          {/* Profile pic placeholder end */}
+  render() {
+    const PaymentButtonText = "Buy me beer";
+    const EditButtonText = "Edit profile";
+    const Username = Cookies.get("username");
+    return (
+      <div className="title_section user_profile_title">
+        <div className="userProfile">
+          <div id="edit_profile" className="edit_profile_section">
+            <Container buttonText={EditButtonText} onSubmit={this.onSubmit} />
+          </div>
+          <div className="profile_pic_section">
+            <h1>Hello, {Username}</h1>
+            {console.log(document.cookie)}
+            {/* Profile pic placeholder begin */}
 
-        </div>
-        <div className='bio_section'>
+            <h1>This is the Profile picture section</h1>
+            <img
+              src="https://image.shutterstock.com/image-vector/default-profile-picture-avatar-photo-260nw-1681253560.jpg"
+              width="262px"
+              alt="this is a placeholder"
+              height="262px"
+              border-radius="50%"
+            />
 
-        {/* Bio placeholder begin */}
+            {/* Profile pic placeholder end */}
+          </div>
+          <div className="bio_section">
+            {/* Bio placeholder begin */}
 
-          <h1>This is the Bio section</h1>
+            <h1>This is the Bio section</h1>
 
-        {/* Bio pic placeholder begin */}
+            {/* Bio pic placeholder begin */}
+          </div>
 
-        </div>
-        <div id='payment' className='payment_section'>
-          <Container formType='payment' buttonText={PaymentButtonText} onSubmit={onSubmit}/>
+          <form onSubmit={this.handleSubmit}>
+            <div id="payment" className="payment_section">
+              <Container
+                formType="payment"
+                buttonText={PaymentButtonText}
+                onSubmit={this.onSubmit}
+              />
+            </div>
+            <label>
+              Pick your gift:
+              <select value={this.state.value} onChange={this.handleChange}>
+                <option value="3" item="muffin">
+                  🧁
+                </option>
+                <option value="5" item="coffee">
+                  🍸
+                </option>
+                <option value="10" item="cocktail">
+                  £10
+                </option>
+                <option value="15" item="tickets">
+                  £15
+                </option>
+                <option value="20" item="trip">
+                  £20
+                </option>
+              </select>
+            </label>
+            <input type="submit" value="Submit" />
+          </form>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default UserProfile;

b48f437c7d1e4f0282b1198d6947ce1eeab1998e
Corinne <corinne.boesch@gmail.com>
2021-10-13 02:06:33 +0100

    added test helper and test server for end to end tesing

--- server/__tests__/createUser.spec.js
+++ server/__tests__/createUser.spec.js
@@ -1,33 +1,31 @@
-const router = require('../routes/users.js');
-const app = require('../server.js'); // Link to your server file
+// const app = require('../routes/users');
+// const supertest = require('supertest');
+// const request = supertest(app);
+// const User = require('../models/user.model');
+// const { setupDB } = require('../test-env/test-helper');
+// setupDB('endpoint-testing', true);
+
+const app = require('../test-env/test-server');
 const supertest = require('supertest');
 const request = supertest(app);
+const User = require('../models/user.model');
 
-// it('get the user test endpoints and displays db content', async () => {
-//   const res = await request.get('/users');
-//   expect(res.status).toBe(200);
-//   expect(res.body).toStrictEqual([
-//     {
-//       _id: '616056fe920263dc9706f129',
-//       username: 'peterpan44',
-//       email: 'bill@gmail.com',
-//       password: '234567891',
-//       createdAt: '2021-10-08T14:34:38.714Z',
-//       updatedAt: '2021-10-08T14:34:38.714Z',
-//       __v: 0,
-//     },
-//   ]);
-// });
+it('get the user test endpoints and displays db content', async () => {
+  const res = await request.get('/users');
+  expect(res.status).toBe(200);
+  expect(res.body).toStrictEqual([]);
+});
 
 it('tests that user can be added and saved', async () => {
-  const res = await (
-    await request.get('/users/register')
-  ).send({
+  const res = await request.post('/users/register').send({
     username: 'David',
-    email: 'sirdavidattenborough@gmail.com',
+    email: 'sirdavidh@gmail.com',
     password: 's4vetheplanet',
   });
-  const user = await router.findOne({ email: 'sirdavidattenborough@gmail.com' });
+
+  expect(res.body.name).toBeTruthy();
+  expect(res.body.email).toBeTruthy();
+  const user = await User.findOne({ email: 'sirdavidh@gmail.com' });
   expect(user.username).toBeTruthy();
-  expect(user.email).toBeTruthy(); 
+  expect(user.email).toBeTruthy();
 });
--- server/test-env/test-server.js
+++ server/test-env/test-server.js
@@ -5,24 +5,22 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.DB_URI);
+mongoose.connect(process.env.DB_URI_TEST);
 const connection = mongoose.connection;
 connection.on('error', console.error.bind(console, 'connection error: '));
 connection.once('open', () => {
-  console.log('MongoDB database connection established successfully');
+  console.log('MongoDB TEST database connection successfull');
 });
 
-const usersRouter = require('./routes/users');
-const stripeRouter = require('./routes/stripe');
+const usersRouter = require('../routes/users');
 
 app.use('/users', usersRouter);
-app.use('/checkout', stripeRouter);
 
+const port = 4444;
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });

f5ca6c317883d81cf65ed0bb3c3511e911d95df8
Farya <farya.hsn@gmail.com>
2021-10-13 13:35:51 +0100

    updated broken change links

--- server/server.js
+++ server/server.js
@@ -11,8 +11,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const usersRouter = require("./routes/users");
-
 mongoose.connect(process.env.DB_URI);
 const connection = mongoose.connection;
 connection.on("error", console.error.bind(console, "connection error: "));

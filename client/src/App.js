afbee3dc80d94d2cf5a317dcb6d0c805a289f5bd
Farya <farya.hsn@gmail.com>
2021-10-14 11:58:02 +0100

    updating file names

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import UserProfile from "./Components/ProfileUser.js";
 import CreateUser from "./Components/RegisterUser";
 import Login from "./Components/LoginUser";
-import messageForm from "./Components/NotUsedMessageForm.js";
+// import messageForm from "./Components/NotUsedMessageForm.js";
 import Navbar from "./Components/Navbar.js";
 import Messages from "./Components/Messages";
 import Form from "./Components/Stripe.js";
@@ -21,7 +21,7 @@ function App() {
       <Route path={`/${Username}`} component={UserProfile} />
       <Route path="/login" component={Login} />
       <Route path="/messages" component={Messages} />
-      <Route path="/m-form" component={messageForm} />
+      {/* <Route path="/m-form" component={messageForm} /> */}
       <Route path="/p-form" component={Form} />
     </Router>
   );

a0d2f73c5efe756261786f2ea600a2e1e346c6af
Corinne <corinne.boesch@gmail.com>
2021-10-14 14:22:56 +0100

    resolving file name issues

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,28 @@
-import "./App.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import UserProfile from "./Pages/ProfileUser";
+import UserProfile from './Pages/ProfileUser';
 
-import CreateUser from "./Pages/RegisterUser.js";
-import Login from "./Pages/LoginUser";
-import Navbar from "./components/Navbar.js";
-import Messages from "./Pages/Messages.js";
+import CreateUser from './Pages/RegisterUser';
+import Login from './Pages/LoginUser';
+import Navbar from './components/Navbar';
+import Messages from './Pages/Messages';
 // import messageForm from "./components/message-form.components.js";
 // import Form from "./components/stripe.components.js";
 
-import Cookies from "js-cookie";
+import Cookies from 'js-cookie';
 
-const Username = Cookies.get("username");
+const Username = Cookies.get('username');
 // const UserId = Cookies.get('id');
 
 function App() {
   return (
     <Router>
       <Navbar />
-      <Route path="/register" component={CreateUser} />
+      <Route path='/register' component={CreateUser} />
       <Route path={`/${Username}`} component={UserProfile} />
-      <Route path="/login" component={Login} />
-      <Route path="/messages" component={Messages} />
+      <Route path='/login' component={Login} />
+      <Route path='/messages' component={Messages} />
       {/* <Route path="/p-form" component={Form} /> */}
       {/* <Route path="/m-form" component={messageForm} /> */}
     </Router>

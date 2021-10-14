c99b09f0995a66c71f44b50d5090404a2526b23d
Farya <farya.hsn@gmail.com>
2021-10-14 11:21:53 +0100

    renamed files

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,28 @@
 import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import UserProfile from './components/user-profile/user-profile.js';
-import CreateUser from './components/user-profile/register-user';
-import Login from './components/user-profile/login-user';
-import messageForm from './components/message-form.components.js';
-import Navbar from './components/navbar.js';
-import Messages from './components/messages';
-import Form from './components/stripe.components.js';
-import Cookies from 'js-cookie';
+import UserProfile from "./Components/ProfileUser.js";
+import CreateUser from "./Components/RegisterUser";
+import Login from "./Components/LoginUser";
+import messageForm from "./Components/NotUsedMessageForm.js";
+import Navbar from "./Components/Navbar.js";
+import Messages from "./Components/Messages";
+import Form from "./Components/Stripe.js";
+import Cookies from "js-cookie";
 
-const Username = Cookies.get('username');
+const Username = Cookies.get("username");
 // const UserId = Cookies.get('id');
 
 function App() {
   return (
     <Router>
       <Navbar />
-      <Route path='/register' component={CreateUser} />
+      <Route path="/register" component={CreateUser} />
       <Route path={`/${Username}`} component={UserProfile} />
-      <Route path='/login' component={Login} />
-      <Route path='/messages' component={Messages} />
-      <Route path='/m-form' component={messageForm} />
-      <Route path='/p-form' component={Form} />
+      <Route path="/login" component={Login} />
+      <Route path="/messages" component={Messages} />
+      <Route path="/m-form" component={messageForm} />
+      <Route path="/p-form" component={Form} />
     </Router>
   );
 }
